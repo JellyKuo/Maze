00566da7e425c8cf8d3f22814a4243f0f4bad4d6
Jelly Kuo <jellykuo1234@gmail.com>
2017-06-08 21:44:47 +0800

    Finished BFS&DFS

---
+++ Maze/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Maze
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.algorithmGroupBox = new System.Windows.Forms.GroupBox();
+            this.AstarRadioButton = new System.Windows.Forms.RadioButton();
+            this.DFSradioButtion = new System.Windows.Forms.RadioButton();
+            this.BFSradioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.initRadioButton = new System.Windows.Forms.RadioButton();
+            this.obsticleRadioButton = new System.Windows.Forms.RadioButton();
+            this.endRadioButton = new System.Windows.Forms.RadioButton();
+            this.gridPanel = new System.Windows.Forms.Panel();
+            this.BFS = new System.Windows.Forms.Timer(this.components);
+            this.DFS = new System.Windows.Forms.Timer(this.components);
+            this.Astar = new System.Windows.Forms.Timer(this.components);
+            this.resetButton = new System.Windows.Forms.Button();
+            this.algorithmGroupBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // algorithmGroupBox
+            // 
+            this.algorithmGroupBox.Controls.Add(this.AstarRadioButton);
+            this.algorithmGroupBox.Controls.Add(this.DFSradioButtion);
+            this.algorithmGroupBox.Controls.Add(this.BFSradioButton);
+            this.algorithmGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.algorithmGroupBox.Name = "algorithmGroupBox";
+            this.algorithmGroupBox.Size = new System.Drawing.Size(102, 63);
+            this.algorithmGroupBox.TabIndex = 0;
+            this.algorithmGroupBox.TabStop = false;
+            this.algorithmGroupBox.Text = "演算法";
+            // 
+            // AstarRadioButton
+            // 
+            this.AstarRadioButton.AutoSize = true;
+            this.AstarRadioButton.Location = new System.Drawing.Point(56, 17);
+            this.AstarRadioButton.Name = "AstarRadioButton";
+            this.AstarRadioButton.Size = new System.Drawing.Size(37, 16);
+            this.AstarRadioButton.TabIndex = 1;
+            this.AstarRadioButton.TabStop = true;
+            this.AstarRadioButton.Text = "A*";
+            this.AstarRadioButton.UseVisualStyleBackColor = true;
+            this.AstarRadioButton.Visible = false;
+            // 
+            // DFSradioButtion
+            // 
+            this.DFSradioButtion.AutoSize = true;
+            this.DFSradioButtion.Location = new System.Drawing.Point(6, 39);
+            this.DFSradioButtion.Name = "DFSradioButtion";
+            this.DFSradioButtion.Size = new System.Drawing.Size(43, 16);
+            this.DFSradioButtion.TabIndex = 0;
+            this.DFSradioButtion.TabStop = true;
+            this.DFSradioButtion.Text = "DFS";
+            this.DFSradioButtion.UseVisualStyleBackColor = true;
+            // 
+            // BFSradioButton
+            // 
+            this.BFSradioButton.AutoSize = true;
+            this.BFSradioButton.Location = new System.Drawing.Point(7, 17);
+            this.BFSradioButton.Name = "BFSradioButton";
+            this.BFSradioButton.Size = new System.Drawing.Size(43, 16);
+            this.BFSradioButton.TabIndex = 0;
+            this.BFSradioButton.TabStop = true;
+            this.BFSradioButton.Text = "BFS";
+            this.BFSradioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.initRadioButton);
+            this.groupBox1.Controls.Add(this.obsticleRadioButton);
+            this.groupBox1.Controls.Add(this.endRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(120, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(113, 63);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "設計";
+            // 
+            // initRadioButton
+            // 
+            this.initRadioButton.AutoSize = true;
+            this.initRadioButton.ForeColor = System.Drawing.Color.Coral;
+            this.initRadioButton.Location = new System.Drawing.Point(6, 39);
+            this.initRadioButton.Name = "initRadioButton";
+            this.initRadioButton.Size = new System.Drawing.Size(47, 16);
+            this.initRadioButton.TabIndex = 0;
+            this.initRadioButton.TabStop = true;
+            this.initRadioButton.Text = "開始";
+            this.initRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // obsticleRadioButton
+            // 
+            this.obsticleRadioButton.AutoSize = true;
+            this.obsticleRadioButton.Location = new System.Drawing.Point(59, 17);
+            this.obsticleRadioButton.Name = "obsticleRadioButton";
+            this.obsticleRadioButton.Size = new System.Drawing.Size(47, 16);
+            this.obsticleRadioButton.TabIndex = 0;
+            this.obsticleRadioButton.TabStop = true;
+            this.obsticleRadioButton.Text = "障礙";
+            this.obsticleRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // endRadioButton
+            // 
+            this.endRadioButton.AutoSize = true;
+            this.endRadioButton.Location = new System.Drawing.Point(6, 17);
+            this.endRadioButton.Name = "endRadioButton";
+            this.endRadioButton.Size = new System.Drawing.Size(47, 16);
+            this.endRadioButton.TabIndex = 0;
+            this.endRadioButton.TabStop = true;
+            this.endRadioButton.Text = "終點";
+            this.endRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // gridPanel
+            // 
+            this.gridPanel.Location = new System.Drawing.Point(11, 82);
+            this.gridPanel.Name = "gridPanel";
+            this.gridPanel.Size = new System.Drawing.Size(302, 301);
+            this.gridPanel.TabIndex = 2;
+            // 
+            // BFS
+            // 
+            this.BFS.Tick += new System.EventHandler(this.BFS_Tick);
+            // 
+            // DFS
+            // 
+            this.DFS.Tick += new System.EventHandler(this.DFS_Tick);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(239, 12);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(74, 63);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "重設";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 395);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.gridPanel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.algorithmGroupBox);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.algorithmGroupBox.ResumeLayout(false);
+            this.algorithmGroupBox.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox algorithmGroupBox;
+        private System.Windows.Forms.RadioButton AstarRadioButton;
+        private System.Windows.Forms.RadioButton DFSradioButtion;
+        private System.Windows.Forms.RadioButton BFSradioButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton initRadioButton;
+        private System.Windows.Forms.RadioButton obsticleRadioButton;
+        private System.Windows.Forms.RadioButton endRadioButton;
+        private System.Windows.Forms.Panel gridPanel;
+        private System.Windows.Forms.Timer BFS;
+        private System.Windows.Forms.Timer DFS;
+        private System.Windows.Forms.Timer Astar;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Maze/MainForm.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maze
+{
+    public partial class MainForm : Form
+    {
+        Button[] bt = new Button[100];
+        Queue<int> q = new Queue<int>();
+        Stack<int> s = new Stack<int>();
+        Stack<int> p = new Stack<int>();
+        int[] pre = new int[100];
+        int step = 0;
+        int goal = 100;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        #region 開始, 生成, 重設
+
+        private void Start(int pos, string Algorithm)
+        {
+            switch (Algorithm)
+            {
+                case "BFS":
+                    Console.WriteLine("BFS algorithm start with pos = " + pos.ToString());
+                    q.Enqueue(pos);
+                    p.Push(pos);
+                    BFS.Enabled = true;
+                    break;
+                case "DFS":
+                    Console.WriteLine("DFS algorithm start with pos = " + pos.ToString());
+                    s.Push(pos);
+                    p.Push(pos);
+                    DFS.Enabled = true;
+                    break;
+                case "Astar":
+                    Console.WriteLine("Astar algorithm start with pos = " + pos.ToString());
+                    break;
+                default:
+                    throw new Exception("我不知道" + Algorithm + "是甚麼");
+            }
+        }
+
+        private void GenerateGrid()
+        {
+            int pos_x = 0;
+            int pos_y = 0;
+
+            for (int a = 0; a < 100; a++)
+            {
+                bt[a] = new Button();
+                bt[a].Name = a.ToString();
+                bt[a].Location = new Point(pos_x, pos_y);
+                bt[a].Size = new Size(30, 30);
+                bt[a].BackColor = Color.AliceBlue;
+                pos_x += 30;
+
+                if (a % 10 == 9)
+                {
+                    pos_x = 0;
+                    pos_y += 30;
+                }
+                bt[a].Click += new EventHandler(ButtonClick);
+                gridPanel.Controls.Add(bt[a]);
+            }
+        }
+
+        private void Reset()
+        {
+            BFS.Enabled = false;
+            DFS.Enabled = false;
+            Astar.Enabled = false;
+            gridPanel.Controls.Clear();
+            GenerateGrid();
+            for (int a = 0; a < 100; a++)
+                pre[a] = -1;
+            BFSradioButton.Checked = true;
+            endRadioButton.Checked = true;
+
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            string Algorithm = "";
+            Button button = (Button)sender;
+            int pos = Convert.ToInt32(button.Name);
+            bt[pos].Enabled = false;
+
+            if (obsticleRadioButton.Checked)
+                bt[pos].BackColor = Color.Black;
+            if (BFSradioButton.Checked)
+                Algorithm = "BFS";
+            if (DFSradioButtion.Checked)
+                Algorithm = "DFS";
+            if (initRadioButton.Checked)
+                Start(pos, Algorithm);
+
+            if (endRadioButton.Checked)
+            {
+                for (int a = 0; a < 100; a++)
+                {
+                    if (bt[a].Text == "G")
+                    {
+                        bt[a].BackColor = Color.AliceBlue;
+                        bt[a].Enabled = true;
+                        bt[a].Text = "";
+                    }
+                }
+                goal = pos;
+                bt[pos].BackColor = Color.Yellow;
+                bt[pos].Text = "G";
+                bt[pos].Enabled = true;
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        #endregion
+
+        #region Timer
+        private void BFS_Tick(object sender, EventArgs e)
+        {
+            if (q.Count() > 0)
+            {
+                int p = q.Dequeue();
+                bt[p].BackColor = Color.Cyan;
+                bt[p].Enabled = false;
+
+                int x = p % 10;
+                int y = p / 10;
+
+                if ((y - 1) >= 0 && bt[p - 10].Enabled && !q.Contains(p - 10))  //Right
+                {
+                    q.Enqueue(p - 10);
+                    pre[p - 10] = p;
+                }
+                if ((y + 1) <= 9 && bt[p + 10].Enabled && !q.Contains(p + 10))  //Up
+                {
+                    q.Enqueue(p + 10);
+                    pre[p + 10] = p;
+                }
+                if ((x - 1) >= 0 && bt[p - 1].Enabled && !q.Contains(p - 1))  //Left
+                {
+                    q.Enqueue(p - 1);
+                    pre[p - 1] = p;
+                }
+                if ((x + 1) <= 9 && bt[p + 1].Enabled && !q.Contains(p + 1))  //Down
+                {
+                    q.Enqueue(p + 1);
+                    pre[p + 1] = p;
+                }
+
+                if (!bt[goal].Enabled)
+                {
+                    BFS.Enabled = false;
+                    MessageBox.Show("BFS在第" + step.ToString() + "步找到終點");
+                    path(goal);
+                }
+                step++;
+            }
+        }
+
+        private void DFS_Tick(object sender, EventArgs e)
+        {
+            if (s.Count() > 0)
+            {
+                int p = s.Pop();
+                bt[p].BackColor = Color.Cyan;
+                bt[p].Enabled = false;
+
+                int x = p % 10;
+                int y = p / 10;
+
+                if ((x + 1) <= 9 && bt[p + 1].Enabled)  //Right
+                {
+                    s.Push(p + 1);
+                    pre[p + 1] = p;
+                }
+                if ((y - 1) >= 0 && bt[p - 10].Enabled)  //Up
+                {
+                    s.Push(p - 10);
+                    pre[p - 10] = p;
+                }
+                if ((x - 1) >= 0 && bt[p - 1].Enabled)  //Left
+                {
+                    s.Push(p - 1);
+                    pre[p - 1] = p;
+                }
+                if ((y + 1) <= 9 && bt[p + 10].Enabled)  //Down
+                {
+                    s.Push(p + 10);
+                    pre[p + 10] = p;
+                }
+
+                if (!bt[goal].Enabled)
+                {
+                    DFS.Enabled = false;
+                    MessageBox.Show("DFS在第" + step.ToString() + "步找到終點");
+                    path(goal);
+                }
+                step++;
+            }
+        }
+
+        #endregion
+        private void path(int p)
+        {
+            if (p != -1 && goal != 100)
+            {
+                bt[p].BackColor = Color.Pink;
+                p = pre[p];
+                path(p);
+            }
+        }
+
+    }
+}
